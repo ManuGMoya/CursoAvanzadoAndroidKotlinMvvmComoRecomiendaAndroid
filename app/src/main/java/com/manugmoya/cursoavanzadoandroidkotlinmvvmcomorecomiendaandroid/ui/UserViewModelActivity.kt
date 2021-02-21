f8a4cfa852874e45c28fc3b68a67115ba1695cd0
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-21 14:28:02 +0100

    User example with ViewModel

--- app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/ui/UserViewModelActivity.kt
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/ui/UserViewModelActivity.kt
@@ -2,13 +2,16 @@ package com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.databinding.ActivityUserViewModelBinding
 import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.utils.User
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.viewmodel.UserViewModel
 
 class UserViewModelActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserViewModelBinding
     var userList: MutableList<User> = mutableListOf()
+    private lateinit var userViewmodel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +23,15 @@ class UserViewModelActivity : AppCompatActivity() {
 
     private fun setUpView() {
 
+        userViewmodel = ViewModelProvider(this).get(UserViewModel::class.java)
+
         binding.btSalvar.setOnClickListener {
             val user = User()
             user.edad = binding.etEdad.text.toString()
             user.nombre = binding.etNombre.text.toString()
 
             userList.add(user)
+            userViewmodel.addUser(user)
         }
 
         binding.btVer.setOnClickListener {
@@ -36,6 +42,13 @@ class UserViewModelActivity : AppCompatActivity() {
             }
 
             binding.tvUser.text = texto
+
+            texto = ""
+            userViewmodel.userList.forEach {
+                texto += it.nombre + " " + it.edad + "\n"
+            }
+
+            binding.tvUserViewModel.text = texto
         }
 
     }
