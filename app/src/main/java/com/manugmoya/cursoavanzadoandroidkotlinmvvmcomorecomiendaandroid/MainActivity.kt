2d71347eee942fa4e4032c3b35e9d4bff3f3aa3b
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-21 10:29:52 +0100

    configure ViewModel

--- app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/MainActivity.kt
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.databinding.ActivityMainBinding
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.ui.ViewModelActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpView()
+    }
+
+    private fun setUpView(){
+
+        binding.btViewModel.setOnClickListener {
+            startActivity(Intent(applicationContext, ViewModelActivity::class.java))
+        }
+
+
+/*        btUserViewModel.setOnClickListener {
+            startActivity(Intent(applicationContext, UserViewModelActivity::class.java)
+            )
+        }
+
+
+        btLiveData.setOnClickListener {
+            startActivity(Intent(applicationContext, LiveDataActivity::class.java))
+        }
+
+        btDataBinding.setOnClickListener {
+            startActivity(Intent(applicationContext, DataBindingActivity::class.java))
+        }
+
+
+        btDBLABinding.setOnClickListener {
+            startActivity(Intent(applicationContext, DBLDActivity::class.java))
+        }*/
+
     }
 }
