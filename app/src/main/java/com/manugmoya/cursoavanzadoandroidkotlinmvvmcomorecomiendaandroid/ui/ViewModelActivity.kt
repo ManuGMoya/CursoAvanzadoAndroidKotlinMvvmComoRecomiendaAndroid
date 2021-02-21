36900406b9cb1007776a68763d46a5766308696a
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-21 10:29:52 +0100

    configure ViewModel

---
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/ui/ViewModelActivity.kt
@@ -0,0 +1,12 @@
+package com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.R
+
+class ViewModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_model)
+    }
+}
