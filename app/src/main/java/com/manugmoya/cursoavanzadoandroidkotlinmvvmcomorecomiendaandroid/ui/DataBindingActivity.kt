debec6695b5d8dafc1c99980d130ea6a2e946a4f
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-27 12:54:27 +0100

    Example DataBinding

---
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/ui/DataBindingActivity.kt
@@ -0,0 +1,28 @@
+package com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.R
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.databinding.ActivityDataBindingBinding
+import com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.utils.User
+
+class DataBindingActivity : AppCompatActivity() {
+
+    lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityDataBindingBinding =
+            DataBindingUtil.setContentView(this@DataBindingActivity, R.layout.activity_data_binding)
+
+        user = User("Pepe", "32")
+
+        binding.user = user
+    }
+
+    private fun setUpView() {
+        TODO("Not yet implemented")
+    }
+}
