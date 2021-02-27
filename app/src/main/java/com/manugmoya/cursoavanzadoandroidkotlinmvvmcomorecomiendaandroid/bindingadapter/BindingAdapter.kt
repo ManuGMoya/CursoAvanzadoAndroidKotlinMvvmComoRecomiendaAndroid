a2e6ece0902b3b80978706d756bf11bcd6c9ba44
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-27 21:01:30 +0100

    Second example bindingAdapter.

--- app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/bindingadapter/BindingAdapter.kt
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/bindingadapter/BindingAdapter.kt
@@ -1,17 +1,30 @@
 package com.manugmoya.cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid.bindingadapter
 
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 object BindingAdapter {
 
     @JvmStatic
     @BindingAdapter("visible")
-    fun setVisibility(view: View, visibility: Boolean){
-        if(visibility){
+    fun setVisibility(view: View, visibility: Boolean) {
+        if (visibility) {
             view.visibility = View.VISIBLE
-        }else{
+        } else {
             view.visibility = View.GONE
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("visibility", "size")
+    fun setSizeAndVisibility(view: TextView, visibility: Boolean, size: Float) {
+        if (visibility) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+
+        view.textSize = size
     }
 }
