faa7622075110aa118951c5b06b3eab3ae18d246
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-27 21:01:30 +0100

    Second example bindingAdapter.

--- app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/viewmodel/DBLDViewModel.kt
+++ app/src/main/java/com/manugmoya/cursoavanzadoandroidkotlinmvvmcomorecomiendaandroid/viewmodel/DBLDViewModel.kt
@@ -8,6 +8,7 @@ class DBLDViewModel : ViewModel() {
 
     val user: MutableLiveData<User> = MutableLiveData()
     var visible: MutableLiveData<Boolean> = MutableLiveData()
+    var size: MutableLiveData<Float> = MutableLiveData(14f)
 
     fun setUser(user: User) {
         this.user.value = user
@@ -28,5 +29,7 @@ class DBLDViewModel : ViewModel() {
         } else {
             visible.value = true
         }
+
+        size.value = size.value!!.toFloat() + 5f
     }
 }
